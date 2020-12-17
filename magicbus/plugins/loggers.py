4e703434b7a4ac0b47dcd48e9e6260bbc97ffbd5
Szymon Lipiński <mabewlun@gmail.com>
2020-12-17 10:50:26 +0100

    Fix code for Python3

--- magicbus/plugins/loggers.py
+++ magicbus/plugins/loggers.py
@@ -2,7 +2,7 @@
 
 import datetime
 import sys
-
+import six
 from magicbus.plugins import SimplePlugin
 
 
@@ -30,7 +30,7 @@ class StreamLogger(SimplePlugin):
             if self.encoding is not None:
                 if isinstance(complete_msg, str):
                     complete_msg = complete_msg.encode(self.encoding)
-
+            complete_msg = six.ensure_str(complete_msg)
             self.stream.write(complete_msg)
             self.stream.flush()
 
