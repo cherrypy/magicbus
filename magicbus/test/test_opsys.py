b39bdc3c335182ac34f44621ee0a70d3ae8b679e
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-03-15 16:11:59 +0100

    Make TestOpsys.test_daemonize a function

--- magicbus/test/test_opsys.py
+++ magicbus/test/test_opsys.py
@@ -31,38 +31,36 @@ class Handler(WebHandler):
 service = WebService(handler_class=Handler)
 
 
-class TestOpsys(object):
+def test_daemonize():
+    if os.name not in ['posix']:
+        return 'skipped (not on posix)'
 
-    def test_daemonize(self):
-        if os.name not in ['posix']:
-            return 'skipped (not on posix)'
+    # Spawn the process and wait, when this returns, the original process
+    # is finished.  If it daemonized properly, we should still be able
+    # to access pages.
+    p = Process([sys.executable, thismodule, 'daemonize'])
+    p.start()
+    pidfile.wait()
+    try:
+        # Just get the pid of the daemonization process.
+        resp = service.do_GET('/pid')
+        assert resp.status == 200
+        page_pid = int(resp.read())
+        with open(pidfile.pidfile, 'rb') as pid_file:
+            actual_pid = pid_file.read()
+        assert ntob(str(page_pid)) == actual_pid
+    finally:
+        # Shut down the spawned process
+        service.do_GET('/exit')
+    pidfile.join()
 
-        # Spawn the process and wait, when this returns, the original process
-        # is finished.  If it daemonized properly, we should still be able
-        # to access pages.
-        p = Process([sys.executable, thismodule, 'daemonize'])
-        p.start()
-        pidfile.wait()
-        try:
-            # Just get the pid of the daemonization process.
-            resp = service.do_GET('/pid')
-            assert resp.status == 200
-            page_pid = int(resp.read())
-            with open(pidfile.pidfile, 'rb') as pid_file:
-                actual_pid = pid_file.read()
-            assert ntob(str(page_pid)) == actual_pid
-        finally:
-            # Shut down the spawned process
-            service.do_GET('/exit')
-        pidfile.join()
-
-        # Wait until here to test the exit code because we want to ensure
-        # that we wait for the daemon to finish running before we fail.
-        p.process.wait()
-        if p.process.returncode != 0:
-            raise AssertionError(
-                'Daemonized parent process returned exit code %s.' %
-                p.process.returncode)
+    # Wait until here to test the exit code because we want to ensure
+    # that we wait for the daemon to finish running before we fail.
+    p.process.wait()
+    if p.process.returncode != 0:
+        raise AssertionError(
+            'Daemonized parent process returned exit code %s.' %
+            p.process.returncode)
 
 
 if __name__ == '__main__':
