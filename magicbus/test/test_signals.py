504c6257d4d077c09281c681d3f2e0ed033618d0
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-03-31 15:27:42 +0200

    ♲ Deduplicate logic in `test_signals`

--- magicbus/test/test_signals.py
+++ magicbus/test/test_signals.py
@@ -1,3 +1,4 @@
+import errno
 import os
 thismodule = os.path.abspath(__file__)
 import sys
@@ -11,13 +12,50 @@ from magicbus.test import Process
 
 
 @pytest.fixture
+def kill():
+    try:
+        return os.kill
+    except AttributeError:
+        pytest.skip('no os.kill')
+
+
+@pytest.fixture
+def signal():
+    return pytest.importorskip('signal')
+
+
+@pytest.fixture
+def SIGHUP(signal):
+    try:
+        return signal.SIGHUP
+    except AttributeError:
+        pytest.skip('no SIGHUP')
+
+
+@pytest.fixture
+def SIGTERM(signal):
+    try:
+        return signal.SIGTERM
+    except AttributeError:
+        pytest.skip('no SIGTERM')
+
+
+@pytest.fixture
 def logfile(request):
     # Ref: https://stackoverflow.com/a/34732269/595220
     test_name = request.node.originalname
-    return os.path.join(
+    path = os.path.join(
         os.path.dirname(thismodule),
         '.'.join((__name__, test_name, 'log')),
     )
+
+    try:
+        os.remove(path)
+    except OSError as os_err:
+        if os_err.errno != errno.ENOENT:
+            raise
+    finally:
+        return path
 
 
 @pytest.fixture
@@ -26,65 +64,48 @@ def pidfile(tmp_path):
     return opsys.PIDFile(bus, str(pid_file_path))
 
 
-def test_SIGHUP_tty(pidfile, logfile):
-    # When not daemonized, SIGHUP should exit the process.
-    try:
-        from signal import SIGHUP
-    except ImportError:
-        return 'skipped (no SIGHUP)'
-
-    try:
-        from os import kill
-    except ImportError:
-        return 'skipped (no os.kill)'
-
+@pytest.fixture
+def tty_process_pid(pidfile, logfile):
     pid_file_path = pidfile.pidfile
-
-    p = Process([sys.executable, thismodule, 'tty', pid_file_path, logfile])
-    p.start()
+    Process([
+        sys.executable, thismodule,
+        'tty', pid_file_path, logfile,
+    ]).start()
     pid = pidfile.wait()
-    kill(pid, SIGHUP)
+    return pid
+
+
+@pytest.fixture
+def daemon_process_pid(pidfile, logfile):
+    pid_file_path = pidfile.pidfile
+    Process([
+        sys.executable, thismodule,
+        'daemonize', pid_file_path, logfile,
+    ]).start()
+    pid = pidfile.wait()
+    return pid
+
+
+def test_SIGHUP_tty(pidfile, tty_process_pid, kill, SIGHUP):
+    # When not daemonized, SIGHUP should exit the process.
+    kill(tty_process_pid, SIGHUP)
     pidfile.join()
 
 
-def test_SIGHUP_daemonized(pidfile, logfile):
+@pytest.mark.skipif(os.name != 'posix', reason='only supported on POSIX')
+def test_SIGHUP_daemonized(pidfile, daemon_process_pid, kill, SIGHUP, SIGTERM):
     # When daemonized, SIGHUP should restart the server.
-    try:
-        from signal import SIGHUP, SIGTERM
-    except ImportError:
-        return 'skipped (no SIGHUP)'
-
-    try:
-        from os import kill
-    except ImportError:
-        return 'skipped (no os.kill)'
-
-    if os.name not in ['posix']:
-        return 'skipped (not on posix)'
-
-    try:
-        os.remove(logfile)
-    except OSError as os_err:
-        import errno
-        if os_err.errno != errno.ENOENT:
-            raise
-
-    pid_file_path = pidfile.pidfile
-
-    p = Process([sys.executable, thismodule, 'daemonize', pid_file_path, logfile])
-    p.start()
-    pid = pidfile.wait()
-    kill(pid, SIGHUP)
+    kill(daemon_process_pid, SIGHUP)
 
     # Give the server some time to restart
     time.sleep(1)
     for _ in range(6):
         new_pid = pidfile.wait(5)
-        if new_pid != pid:
+        if new_pid != daemon_process_pid:
             break
         time.sleep(5)
     assert new_pid is not None
-    assert new_pid != pid
+    assert new_pid != daemon_process_pid
     kill(new_pid, SIGTERM)
     pidfile.join()
 
@@ -94,50 +115,17 @@ def test_SIGHUP_daemonized(pidfile, logfile):
     reason='The non-daemonized process cannot detect the TTY on Windows for some reason',
     run=False,
 )
-def test_SIGTERM_tty(pidfile, logfile):
+def test_SIGTERM_tty(pidfile, tty_process_pid, kill, SIGTERM):
     # SIGTERM should shut down the server whether daemonized or not.
-    try:
-        from signal import SIGTERM
-    except ImportError:
-        return 'skipped (no SIGTERM)'
 
-    try:
-        from os import kill
-    except ImportError:
-        return 'skipped (no os.kill)'
-
-    pid_file_path = pidfile.pidfile
-
-    # Spawn a normal, undaemonized process.
-    p = Process([sys.executable, thismodule, 'tty', pid_file_path, logfile])
-    p.start()
-    pid = pidfile.wait()
-    kill(pid, SIGTERM)
+    kill(tty_process_pid, SIGTERM)
     pidfile.join()
 
 
-def test_SIGTERM_daemonized(pidfile, logfile):
+@pytest.mark.skipif(os.name != 'posix', reason='only supported on POSIX')
+def test_SIGTERM_daemonized(pidfile, daemon_process_pid, kill, SIGTERM):
     # SIGTERM should shut down the server whether daemonized or not.
-    try:
-        from signal import SIGTERM
-    except ImportError:
-        return 'skipped (no SIGTERM)'
-
-    try:
-        from os import kill
-    except ImportError:
-        return 'skipped (no os.kill)'
-
-    if os.name not in ['posix']:
-        return 'skipped (not on posix)'
-
-    pid_file_path = pidfile.pidfile
-
-    # Spawn a daemonized process and test again.
-    p = Process([sys.executable, thismodule, 'daemonize', pid_file_path, logfile])
-    p.start()
-    pid = pidfile.wait()
-    kill(pid, SIGTERM)
+    kill(daemon_process_pid, SIGTERM)
     pidfile.join()
 
 
