6e94740acbbe8b12a28d8f2ad4ac9dc5fc28e867
Robert Brewer <fumanchu@aminus.org>
2025-11-13 08:39:29 -0800

    Fix isAlive/is_alive for various Pythons

--- magicbus/plugins/lifecycle.py
+++ magicbus/plugins/lifecycle.py
@@ -16,6 +16,11 @@ else:
     except AttributeError:
         max_files = 1024
 
+if hasattr(threading.currentThread(), "is_alive"):
+    is_alive = lambda t: t.is_alive()
+else:
+    is_alive = lambda t: t.isAlive()
+
 from magicbus import plugins
 
 
@@ -39,13 +44,17 @@ class ThreadWait(plugins.SimplePlugin):
 
     def EXIT(self):
         # Waiting for ALL child threads to finish is necessary on OS X.
-        # See https://github.com/cherrypy/cherrypy/issues/581.
+        # See http://www.cherrypy.org/ticket/581.
         # It's also good to let them all shut down before allowing
         # the main thread to call atexit handlers.
-        # See https://github.com/cherrypy/cherrypy/issues/751.
+        # See http://www.cherrypy.org/ticket/751.
         self.bus.log('Waiting for child threads to terminate...')
+        curthread = threading.currentThread()
         for t in threading.enumerate():
-            if t == threading.current_thread() or not t.is_alive():
+            if t is curthread:
+                continue
+
+            if not is_alive(t):
                 continue
 
             # Note that any dummy (external) threads are always daemonic.
